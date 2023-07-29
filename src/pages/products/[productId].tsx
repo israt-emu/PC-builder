c70976eaaae5e35a78788f76271a691269486620
unknown <isratjahan.emu1998@gmail.com>
2023-07-29 10:58:24 +0600

    git push

--- src/pages/products/[productId].tsx
+++ src/pages/products/[productId].tsx
@@ -5,12 +5,15 @@ import {NextPageWithLayout} from "../_app";
 import RootLayout from "@/components/layout/RootLayout";
 import Image from "next/image";
 import ProductBadge from "@/components/ui/ProductBadge";
-import ProductFeatures from "@/components/ui/ProductFeatures";
+import {FaStar} from "react-icons/fa";
+
+// import ProductFeatures from "@/components/ui/ProductFeatures";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 
 const ProductDeatils: NextPageWithLayout<ISingleProductProps> = ({product}: ISingleProductProps) => {
   return (
     <section className="">
-      <div className="container flex flex-col mx-auto lg:flex-row">
+      <div className="container flex flex-col mx-auto lg:flex-row ">
         <div className="w-full lg:w-1/3">
           <Image src={product?.image} alt="product" width={400} height={400} />
         </div>
@@ -21,8 +24,46 @@ const ProductDeatils: NextPageWithLayout<ISingleProductProps> = ({product}: ISin
             <ProductBadge text="Status:" value={`${product?.status}`} />
             <ProductBadge text="Category:" value={`${product?.category}`} />
           </div>
-          <ProductFeatures features={product?.key_features} />
+          {/* <ProductFeatures features={product?.key_features} /> */}
         </div>
+      </div>
+      <div className="py-6 bg-secondary">
+        <Tabs defaultValue="description" className="w-10/12 mx-auto ">
+          <TabsList className="rounded-none grid grid-cols-3 lg:grid-cols-6 bg-transparent border-b border-gray-500 pb-2">
+            <TabsTrigger value="description" className="focus:bg-accent">
+              Description
+            </TabsTrigger>
+            <TabsTrigger value="rating">Rating</TabsTrigger>
+            <TabsTrigger value="reviews">Reviews</TabsTrigger>
+          </TabsList>
+          <TabsContent value="description" className="lg:p-4 mt-3">
+            {product?.description}
+          </TabsContent>
+          <TabsContent value="rating" className="lg:p-6 mt-3">
+            <div className="flex items-center gap-2">
+              <p className="text-primary font-semibold">Average Rating:</p>
+              <FaStar className="text-yellow-500" />
+              <p className="text-accent">{(product?.reviews!?.reduce((acc, review) => acc + review.rating, 0) / product?.reviews.length).toFixed(1)}</p>
+            </div>
+            <p className="text-primary font-semibold">Individual Rating:</p>
+            {product?.reviews?.map((review, i) => (
+              <div key={i} className="flex items-center">
+                <p>{review?.name}: </p>
+                <p className="flex items-center gap-1">
+                  ( {review?.rating}) <FaStar />
+                </p>
+              </div>
+            ))}
+          </TabsContent>
+          <TabsContent value="reviews" className="lg:p-6 mt-3">
+            {product?.reviews?.map((review, i) => (
+              <div key={i} className="flex items-center gap-2">
+                <p>{review?.name}: </p>
+                <p>{review?.review}</p>
+              </div>
+            ))}
+          </TabsContent>
+        </Tabs>
       </div>
     </section>
   );
